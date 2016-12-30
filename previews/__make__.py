442f176776025698e8931a8d9ad502cab4a2101b
Kevin Zhang <kevz@mit.edu>
2016-12-30 10:47:03 -0500

    Updated preview to use new map helper

--- previews/__make__.py
+++ previews/__make__.py
@@ -3,19 +3,15 @@ from os import path
 MY_DIR = path.dirname(path.abspath(__file__))
 sys.path.insert(0, path.abspath(MY_DIR + '/..'))
 
-import konics.maps
+from konics.maps import *
 from konics.core import *
 from konics.utils import *
 from scipy.misc import imsave
 
-for map_id in konics.maps.MAP_IDS:
+for map_id in MAP_IDS:
     print(map_id)
-    item = getattr(konics.maps, map_id)
-
-    world = World()
-    track = Track(item.x_t, item.y_t, item.e_t)
-    track.add_to(world)
-
+    
+    world, track = make_wt(map_id)
     drive = Drive(world)
     drive.set_pose(track.get_pose(0.0))
 
