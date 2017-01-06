008d3d28e2d3c37c2929789dd680d65453d2d92e
Kevin Zhang <kevz@mit.edu>
2017-01-06 07:31:34 -0500

    Added method to evaluate pose

--- konics/core/track.py
+++ konics/core/track.py
@@ -1,14 +1,22 @@
 import numpy as np
-from math import atan2
 from random import random
+from math import atan2, sqrt, pi
 
 Z_HAT = np.array([0.0, 0.0, 1.0])
 P_MISSING_CONE = 0.1
+VEHICLE_WIDTH = 5.0 + random() * 5.0
 CONE_DISTANCE = 15.0 + random() * 5.0
-VEHICLE_WIDTH = 15.0 + random() * 5.0
 
 def D(f_t, t, eps=1e-3):
     return (f_t(t + eps) - f_t(t - eps)) / (2.0 * eps)
+
+def M(f_t, s_t, e_t, res=10000):
+    best, best_t = f_t(s_t), s_t
+    for t in np.linspace(s_t, e_t, num=res):
+        if f_t(t) < best:
+            best_t = t
+            best = f_t(t)
+    return t, best
 
 class Track:
     
@@ -32,7 +40,7 @@ class Track:
         for t in np.linspace(0.0, self.e_t, num=10000):
             x, y, a, dx, dy = self.get_full_pose(t)
             normal = np.cross(np.array([dx, dy, 0]), Z_HAT)
-            dx, dy, _ = normal / np.linalg.norm(normal) * (VEHICLE_WIDTH / 2.0)
+            dx, dy, _ = normal / np.linalg.norm(normal) * VEHICLE_WIDTH
 
             left = True
             if abs(x + dx - left_cone[0]) < CONE_DISTANCE:
@@ -51,3 +59,14 @@ class Track:
                 right_cone = (x - dx, y - dy)
                 if random() > P_MISSING_CONE:
                     world.add_cone(x - dx, y - dy)
+
+    def evaluate(self, pose):
+        x, y, a = pose
+        def distance(t):
+            return sqrt((self.x_t(t) - x) ** 2 + (self.y_t(t) - y) ** 2)
+        t, d = M(distance, 0.0, self.e_t)
+        _, _, aa = self.get_pose(t)
+        print(a, aa)
+        on_track = True if d < VEHICLE_WIDTH else False
+        right_dir = True if abs(aa - a) < pi / 2.0 else False
+        return on_track, right_dir
