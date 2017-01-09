35d54df768def139631957b60f66933cdc940277
Kevin Zhang <kevz@mit.edu>
2017-01-09 12:37:40 -0500

    Added check for experimental rendering server

--- konics/core/__init__.py
+++ konics/core/__init__.py
@@ -1,3 +1,12 @@
 from .track import *
 from .drive import *
 from .world import *
+
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.settimeout(0.2)
+try:
+    s.connect(("localhost", 3000))
+    print("Detected rendering server. Using hardware accelerated rendering engine.")
+except:
+    print("Failed to detect rendering server. Falling back to ray-tracing renderer.")
