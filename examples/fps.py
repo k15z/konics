ca56204fd1a9fcba8afe53e0048e0b6e043e6ad2
Kevin Zhang <kevin.2015.zhang@gmail.com>
2017-01-10 08:54:03 -0500

    FPS example

--- examples/fps.py
+++ examples/fps.py
@@ -1,16 +1,17 @@
 import sys
 from os import path
+from tqdm import tqdm
+from random import choice
 MY_DIR = path.dirname(path.abspath(__file__))
 sys.path.insert(0, path.abspath(MY_DIR + '/..'))
 
-from tqdm import tqdm
 from konics.maps import *
 from konics.core import *
 from konics.utils import *
 from scipy.misc import imsave
 
-world, track = make_wt("kilo")
+world, track = make_wt(choice(MAP_IDS))
 drive = Drive(world)
-for t in tqdm(range(100)):
+for t in tqdm(range(1000)):
     drive.set_pose(track.get_pose(t / 100.0))
     drive.render()
